a672c5959a064fcc7ec3cbb44efb54ba63caec5d
Johnny Halife <johnny@southworks.com>
2017-12-18 13:57:37 -0800

    Initial Commit

---
+++ az-mock-video-uploader/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzMockVideoUploader
+{
+    class Program
+    {
+        private static readonly string MediaFile = @"Media\BigBuckBunny.mp4";
+
+        private static readonly string AzStorageConnectionString = 
+                ConfigurationManager.AppSettings["AzStorageConnectionString"];
+
+        private static readonly string AzStorageContainerName =
+                ConfigurationManager.AppSettings["AzStorageContainerName"];
+
+        public static void Main(string[] args)
+        {
+            // Get the Az Storge Account
+            var storageAccount = CloudStorageAccount.Parse(AzStorageConnectionString);
+
+            // Create the blob client to interact with Storage Account
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get a container reference 
+            var container = blobClient.GetContainerReference(AzStorageContainerName);
+
+            // Ensure that the container exists
+            container.CreateIfNotExists();
+
+            // Retrieve reference to a blob named "BigBuckBunny".
+            var blockBlob = container.GetBlockBlobReference("BigBuckBunny");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(MediaFile))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+    }
+}
